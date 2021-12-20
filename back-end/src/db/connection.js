7a940db77b7fd7f659e1b2fc35f2222661e1613b
Todd White <todddeakron@gmail.com>
2021-12-20 14:07:11 -0500

    first commit

--- back-end/src/db/connection.js
+++ back-end/src/db/connection.js
@@ -1,5 +1,5 @@
 const environment = process.env.NODE_ENV || "development";
 const config = require("../../knexfile")[environment];
 const knex = require("knex")(config);
-
+console.log("environment", environment);
 module.exports = knex;
